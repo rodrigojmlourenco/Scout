259fbef2d0dd300a3179c40b06a987fc9dc1d697
rodrigojmlourenco <rodrigo.jm.lourenco@gmail.com>
2015-06-02 18:39:06 +0100

    Criada a AdaptiveOffloadingTaggingStage

--- android/Scout/app/src/main/java/pt/ulisboa/tecnico/cycleourcity/scout/offloading/stages/AdaptiveOffloadingTaggingStage.java
+++ android/Scout/app/src/main/java/pt/ulisboa/tecnico/cycleourcity/scout/offloading/stages/AdaptiveOffloadingTaggingStage.java
@@ -1,14 +1,50 @@
 package pt.ulisboa.tecnico.cycleourcity.scout.offloading.stages;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.ideaimpl.patterns.pipeline.PipelineContext;
 import com.ideaimpl.patterns.pipeline.Stage;
 
+import pt.ulisboa.tecnico.cycleourcity.scout.mobilesensing.pipeline.SensorPipelineContext;
+import pt.ulisboa.tecnico.cycleourcity.scout.mobilesensing.pipeline.sensor.ConfigurationCaretaker;
+
 /**
- * Created by rodrigo.jm.lourenco on 01/06/2015.
+ * The AdaptiveOffloadingTaggingStage is a custom stage, designed to tag each sample with the
+ * corresponding missing stages, i.e., the stages that were offloaded.
+ * <br>
+ * By tagging each sensor sample with his missing stages, the server becomes able to
+ * adjust itself in order to apropriately process those samples.
  */
 public class AdaptiveOffloadingTaggingStage implements Stage {
+
+    private ConfigurationCaretaker caretaker;
+
+    public AdaptiveOffloadingTaggingStage(ConfigurationCaretaker caretaker){
+        this.caretaker = caretaker;
+    }
+
     @Override
     public void execute(PipelineContext pipelineContext) {
 
+        if(caretaker.isOriginal())
+            return;
+
+        SensorPipelineContext ctx = (SensorPipelineContext)pipelineContext;
+        JsonObject[] input = ctx.getInput();
+
+        JsonParser parser = new JsonParser();
+        for(JsonObject sample : input){
+
+            JsonArray missingStages = new JsonArray();
+            for (Stage missing : caretaker.getMissingStages()){
+                JsonElement missingStageName = parser.parse(((ProfilingStageWrapper) missing).getStageClass().getName());
+                missingStages.add(missingStageName);
+            }
+
+            //TODO: corrigir "_config" não devia estar hardcoded
+            sample.add("_config", missingStages);
+        }
     }
 }
